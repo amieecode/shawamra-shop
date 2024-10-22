4786fab70c05bf992f2dfffcc1d5a7c823a37e54
amieecode <enwelumchiamaka@gmail.com>
2024-10-22 23:52:11 +0100

    add

--- frontend/src/Components/Home/Recommended.jsx
+++ frontend/src/Components/Home/Recommended.jsx
@@ -5,6 +5,16 @@ const Recommended = () => {
     <div className='py-10'>
       <div className='container'>
         <h2 className='text-2xl font-bold uppercase text-gray-300'>Our Recommended Shawarma</h2>
+        {/**/}
+        <div>
+          <img />
+        </div>
+
+        {/**/}
+        <div>
+          <h2>Beef Shawarma Wrap</h2>
+        </div>
+
       </div>
     </div>
   )
