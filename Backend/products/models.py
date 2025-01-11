0b6720f7b03ac86d41ceb2688404f1a365ace0af
amieecode <enwelumchiamaka@gmail.com>
2025-01-11 23:21:09 +0100

    Adding product models

--- Backend/products/models.py
+++ Backend/products/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class product(models.Model):
+class Product(models.Model):
     # Define a product with fields 
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=225)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/', null=True, blank=True) # Upload images to the products/' folder
-
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='products/', null=True, blank=True) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
