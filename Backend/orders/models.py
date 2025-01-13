43036fa8d7bcc0b1aa40faedf69a91188fc61a80
amieecode <enwelumchiamaka@gmail.com>
2025-01-13 15:45:22 +0100

    Created an order app

---
+++ Backend/orders/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from .models import Product
+
+# Create your models here.
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="orders")
+    quantity = models.PositiveIntegerField()
+    customer_name = models.CharField(max_length=225)
+    customer_email = models.EmailField()
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"order {self.id} - {self.customer_name}"
+    
