eda33049d6412d1b8bd8d9d85c988196b034de43
amieecode <enwelumchiamaka@gmail.com>
2025-03-23 16:49:32 +0100

    Addeda redux action for the cart

---
+++ Frontend/src/redux/slices/CartSlice.jsx
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Load cart from the local storage
+const storedCart = JSON.parse(localStorage.getItem("cart")) || [];
+
+const cartSlice = createSlice({
+    name : "cart",
+    initialState: {
+        cart: storedCart, // initial state for local storage
+    },
+    reducers: {
+        addToCart: (state, action) => {
+            state.items.push(action.payload);
+            localStorage.setItem("cart", JSON.stringify(state.items)); // update local storage
+        },
+        removeFromCart: (state, action) => {
+            state.items = state.items.filter((item) => item.id !== action.payload.id);
+            localStorage.setItem("cart", JSON.stringify(state.items));
+        },
+        clearCart: (state) => {
+            state.items=[];
+            localStorage.removeItem("cart");
+        },
+    },
+});
