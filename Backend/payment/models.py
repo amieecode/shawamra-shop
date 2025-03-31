72b1cc79e36fb7acede175edfe4dab4674c044d7
amieecode <enwelumchiamaka@gmail.com>
2025-03-31 03:22:13 +0100

    payment done

--- Backend/payment/models.py
+++ Backend/payment/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from orders.models import Order
 
 # Create your models here.
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    stripe_payment_id = models.CharField(max_length=100)
+    status = models.CharField(max_length=20, choices=[('pending', 'Pending'), ('completed', 'Completed')])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Payment {self.id} - {self.status}"
--- Backend/payment/views.py
+++ Backend/payment/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+import stripe
+from django.conf import settings
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Order, Payment
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 # Create your views here.
+@csrf_exempt
+def create_checkout_session(request):
+    if request.method == "POST":
+        order_id = request.POST.get("order_id")
+        order = Order.objects.get(id=order_id)
+
+        session = stripe.checkout.Session.create(
+            payment_method_type=["card"],
+            line_items = [{
+                "price_data":{
+                    "currency": "usd",
+                    "product_data": {"data": "Shawarma Order"},
+                    "unit_amount": int(order.total_price * 100),  #Convert to Cents
+                },
+                "quantity": 1,
+            }],
+            mode = "payment",
+            success_url = "http://localhost:3000/payment-success",
+            cancel_url =  "http://localhost:3000/payment-cancel",
+        )
+
+        payment = Payment.objects.create(
+            user = order.user,
+            order = order,
+            amount = order.total_price,
+            stripe_payment_id = session.id,
+            status = 'pending'
+        )
+
+        return JsonResponse({"id": session.id})
+    return JsonResponse({"error": "Invalid request"}, status=400)
