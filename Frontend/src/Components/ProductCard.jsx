7734cf2476fd58c0ee7a604e65249cfa79549bca
amieecode <enwelumchiamaka@gmail.com>
2025-04-18 23:57:31 +0100

    Intergrating product to the cart

---
+++ Frontend/src/Components/ProductCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { addToCart } from '../api/cartApi'
+
+const ProductCard = ({ product }) => {
+    const handleAddToCart = async (productId) =>{
+        try {
+            await addToCart(productId, 1);
+            alert("Added to cart!");
+        } catch(err){
+            console.error(err);
+            alert("failed to add to cart");
+        }
+    };
+
+  return (
+    <div className='bg-white text-black p-4 rounded-lg shadow-lg w-full sm:w-[300px]'>
+      <img src={product.image} alt={product.name} className='w-full h-40 object-cover rounded-md' />
+      <h3 className='text-xl font-bold mt-2'>{product.name}</h3>
+        <p className='text-sm'>{product.description}</p>
+        <p className='font-semibold mt-2'>${product.price}</p>
+        <button
+            onClick={() => handleAddToCart(product.id)}
+            className='brand-btn w-full'
+        >
+            Add to cart
+        </button>
+    </div>
+  )
+}
+
+export default ProductCard
---
+++ Frontend/src/pages/ProductPAge.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import ProductCard from '../Components/ProductCard';
+import api from '../axios';
+
+const ProductPAge = () => {
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        try {
+            const res = await api.get("/products/");
+            setProducts(res.data);
+        } catch (err) {
+            console.error("Error fetching products:", err);
+        }
+    };
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+  return (
+    <div className='min-h-screen bg-black text-white p-8'>
+      <h2 className='text-3xl font-bold mb-6'>Our Shawarma Menu</h2>
+      <div className='grid grid-col=-21 sm;grid-cols-2 lg:grid-col-3 gap-6'>
+        {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default ProductPAge;
