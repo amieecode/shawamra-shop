9e50b2e273d2c4dad9e77c212d8adda377c07251
amieecode <enwelumchiamaka@gmail.com>
2025-05-05 23:54:46 +0100

    Working on the state management

--- Frontend/src/Components/Layout/NavBar.jsx
+++ Frontend/src/Components/Layout/NavBar.jsx
@@ -139,7 +139,7 @@ const NavBar = () => {
 
                 {/* Badge */}
                 <span className='absolute -top-1 -right-1 bg-brand text-white text-xs font-bold px-1 py-[0.02rem] rounded-full'>
-                  2
+                  3
                 </span>
               </div>
               <span>Cart</span>
